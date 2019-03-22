8d682ee6b614bade948a032ef938726a53c10604
Abby Gottlich <abby.gottlich@gmail.com>
2019-03-21 19:28:52 -0500

    everything showing up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+let Tables = () => <div>Tables stuff</div>;
+let Charts = () => <div>Charts stuff</div>;
+let Things = () => <div>Things stuff</div>;
 
 function App() {
   return (
-    <div> 
-      <div id="wrapper">
-        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <TopNav />
-          <SideNav />
-        </nav>
-        <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+    <Router>
+      <div>
+        <div id="wrapper">
+          <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <TopNav />
+            <SideNav />
+          </nav>
+          <div style={{ backgroundColor: "white" }}>
+            {/* PUT YOUR ROUTES HERE */}
+
+            {/* every path in your entire app will go in App.js */}
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/things" component={Things} />
+            <Route path="/charts" component={Charts} />
+            <Route path="/tables" component={Tables} />
+
+          </div>
         </div>
       </div>
-    </div>
-
+    </Router>
   );
 }
 
